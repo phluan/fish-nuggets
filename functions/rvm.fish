74ca35e11442543566ae433fc3cace225a2c7af1
Daniel P. Clark <6ftdan@gmail.com>
2017-02-03 20:30:35 -0500

    Use regex flag for grep
    `grep` has long supported a flag to explicitly state regex is being used with `-E`.  This has been around for well beyond 10 years.  No behavior changes other than providing a fix for issue 17.
    
    Fixes #17

--- functions/rvm.fish
+++ functions/rvm.fish
@@ -4,7 +4,7 @@ function rvm --description='Ruby enVironment Manager'
   bash -c 'PATH=$GEM_HOME/bin:$PATH;source $(which rvm | sed "s/bin/scripts/"); rvm "$@"; status=$?; env > "$0"; exit $status' $env_file $argv
 
   # apply rvm_* and *PATH variables from the captured environment
-  and eval (grep '^rvm\|^[^=]*PATH\|^GEM_HOME' $env_file | grep -v '_clr=' | sed '/^[^=]*PATH/s/:/" "/g; s/^/set -xg /; s/=/ "/; s/$/" ;/; s/(//; s/)//')
+  and eval (grep -E '^rvm\|^[^=]*PATH\|^GEM_HOME' $env_file | grep -v '_clr=' | sed '/^[^=]*PATH/s/:/" "/g; s/^/set -xg /; s/=/ "/; s/$/" ;/; s/(//; s/)//')
   # needed under fish >= 2.2.0
   and set -xg GEM_PATH (echo $GEM_PATH | sed 's/ /:/g')
 
